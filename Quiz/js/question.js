8a5a26c248d18145a87fac67871441103cf1bd54
inesdev <inessouzasp@gmail.com>
2021-01-15 14:36:55 -0300

    Quiz
    Quiz feito em HTML, CSS e JavaScript.

---
+++ Quiz/js/question.js
@@ -0,0 +1,54 @@
+/* Array com as questões */
+
+const quiz = [
+    {
+        q:'Sob os efeitos do álcool os jovens ficam mais propensos a quais tipos de comportamentos?',
+        options:['Brigas e sexo desprotegido.','Alegria e felicidade.','Não altera o comportamento.'],
+        answer:0
+    },
+    {
+        q:'Quais dessas são consequências ao corpo humano devido ao uso abusivo do álcool?',
+        options:['Enfraquecimento significativo das defesas do corpo contra infecções.','Problemas articulares.','Aumento do colesterol.'],
+        answer:0
+    },
+    {
+        q:'Em média quantas mortes no mundo acontecem por ano devido ao uso nocivo do álcool?',
+        options:['100 mil mortes.','500 mil mortes.','3 milhões de mortes.'],
+        answer:2
+    },
+    {
+        q:'Quais destas doenças podem ser causadas pelo álcool?',
+        options:['Dengue','Cirrose','Reumatismo'],
+        answer:1
+    },
+    {
+        q:'Consumir álcool durante a gravidez pode implicar em danos ao cérebro do feto, quais são estes danos?',
+        options:['Cérebro com tamanho inferior ao normal para a idade do feto e quantidade de neurônios reduzidas.','Cérebro com tamanho maior ao normal para a idade do feto.','Hemorragia Cerebral.'],
+        answer:0
+    },
+    {
+        q:'A dependência do álcool afeta não só a saúde, como também o convívio social, de que forma isso pode ocorrer?',
+        options:['Gasto de dinheiro com o álcool, desfalcando o orçamento familiar e surgimento de violência doméstica.','Aumento do convívio com a família.','Aumento do número de amizades.'],
+        answer:0
+    },
+    {
+        q:'Qual tipo de violência aumentou durante a pandemia de Covid-19 e está ligada ao consumo de álcool?',
+        options:['Violência contra animais.','Violência doméstica.','Violência contra moradores de rua.'],
+        answer:1
+    },
+    {
+        q:'Quais são os efeitos imediatos que o consumo de álcool causa ao corpo humano?',
+        options:['Dificuldades motoras, fala arrastada e raciocínio lento.','Raciocínio rápido e mais disposição.','Melhora na memória e na fala.'],
+        answer:0
+    },
+    {
+        q:'O BPE consiste no consumo de grande quantidade de álcool em um curto espaço de tempo, que é mais comum entre os jovens e está associado a riscos como acidentes e violência, o que significa esta sigla?',
+        options:['Beber Pesado Episódico','Beber Para Esquecer','Bebendo Por Episódio'],
+        answer:0
+    },
+    {
+        q:'Quais destes meios de comunicação mais expõe adolescentes e jovens as propagandas de álcool e está associado ao início precoce e ao aumento do consumo de álcool entre os mesmos?',
+        options:['Revistas e Jornais','Radio e TV','Redes Sociais'],
+        answer:2
+    },
+]
