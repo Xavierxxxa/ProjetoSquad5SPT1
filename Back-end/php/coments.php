f2f5e9c9291b6ae680a65500d416df9bc7126e83
Xavierxxxa <xavier.g13@outlook.com>
2021-01-18 18:00:00 -0300

    Regisro e listagem de comentários
    Realização de Insert e Select na tabela de comentários.

---
+++ Back-end/php/coments.php
@@ -0,0 +1,23 @@
+<?php
+
+require "Connection.php";
+
+$conection = mysqli_connect($servername, $username, $password, $database);
+
+$sqlSelect = "SELECT * FROM squad5.comentarios;";
+$stmt = $conection -> query($sqlSelect);
+
+foreach($stmt as $key => $value){
+    echo "<h1> Avaliação </h1>";
+    echo "Nome: ";
+    echo $value['nome'];
+    echo "<br/>";
+    echo "Mensagem: ";
+    echo $value['msg'];
+    echo "<br/>";
+    echo "Data: ";
+    echo $value['hora'];
+    echo "<hr/>";
+}
+
+//Teste para ilustração.
