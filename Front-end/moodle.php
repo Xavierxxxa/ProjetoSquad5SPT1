e820c330fe6f814d0f565346e8e81e9cbd5d5a63
nansfromspace <nathaliegiuliani@hotmail.com>
2021-01-20 12:46:48 -0200

    front end
    mudancas no moodle, cadastro e index

--- Front-end/moodle.php
+++ Front-end/moodle.php
@@ -1,135 +1,190 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0 responsive">
-    <title>AVA Conscire</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-    <link rel="stylesheet" href="./Css/style2.css"> <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!--wrapper start-->
-    <div class="wrapper">
-<!--header menu start-->
-<div class="header">
-    <div class="header-menu">
-        <div class="title"> AVA <span>CONSCIRE</span></div>
-        <div class="sidebar-btn">
-            <i class="fas fa-bars"></i>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> 
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+        <link rel="stylesheet" href="css/style-moodle.css">
+
+        <title>AVA CONSCIRE</title>
+    </head>
+    <body id="body-pd">
+        <header class="header" id="header">
+            <div class="header__toggle">
+                <i class='bx bx-menu' id="header-toggle"></i>
             </div>
-            <ul>
-                <li><a href="#"><i class="fas fa-search"></i></a></li>
-                <li><a href="#"><i class="fas fa-bell"></i></a></li>
-                <li><a href="../Back-end/php/sair.php"><i class="fas fa-power-off"></i></a></li>
-            </ul>
+
+            <div class="header__img">
+                <img src="" alt="">
+            </div>
+        </header>
+
+        <div class="l-navbar" id="nav-bar">
+            <nav class="nav">
+                <div>
+                    <a href="#" class="nav__logo">
+                        <i class='bx bx-layer nav__logo-icon'></i>
+                        <span class="nav__logo-name">Conscire</span>
+                    </a>
+
+                    <div class="nav__list">
+                        <a href="#" class="nav__link active">
+                        <i class='bx bx-grid-alt nav__icon' ></i>
+                            <span class="nav__name">Painel Geral</span>
+                        </a>
+
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-user nav__icon' ></i>
+                            <span class="nav__name">Perfil</span>
+                        </a>
+                        
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-brain nav__icon' ></i>
+                            <span class="nav__name">Quiz</span>
+                        </a>
+
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-brain nav__icon' ></i>
+                            <span class="nav__name">História das bedidas</span>
+                        </a>
+
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-brain nav__icon' ></i>
+                            <span class="nav__name">Teste Audit</span>
+                        </a>
+
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-trophy nav__icon' ></i>
+                            <span class="nav__name">Resultados</span>
+                        </a>
+
+
+                    </div>
+                </div>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-log-out nav__icon' ></i>
+                    <span class="nav__name">Sair</span>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-log-out nav__icon' ></i>
+                    <span class="nav__name">Deletar conta</span>
+                </a>
+            </nav>
         </div>
-    </div>  
-<!--header menu end-->
-<!--sidebar start-->
-<div class="sidebar">
-    <div class="sidebar-menu">
-        <center class="profile">
-            <img src="./imagens/avatar.png" alt="">
-            <p>Seja bem vindo(a)!</p>
-        </center>
-    <li class="item">
-        <a href="#" class="menu-btn">
-            <i class="fas fa-desktop"></i><span>Dashboard</span>
-        </a>
-    </li>
-    <li= class="item" id="profile">
-        <a href="#profile" class="menu-btn">
-            <i class="fas fa-user-circle"></i><span>Perfil<i class="fas fa-chevron-down drop-down"></i></span>
-        </a>
-        <div class="sub-menu">
-            <a href="#"><i class="fas fa-image"></i><span>Mudar a foto</span></a>
-            <a href="#"><i class="fas fa-address-card"></i><span>Dados pessoais</span></a>
-        </div>
-    </li>
-    <li= class="item" id="messages">
-        <a href="#messages" class="menu-btn">
-            <i class="fas fa-book"></i><span>Conteúdo<i class="fas fa-chevron-down drop-down"></i></span>
-        </a>
-        <div class="sub-menu">
-            <a href="#"><i class="fas fa-book"></i><span>Quiz</span></a>
-            <a href="#"><i class="fas fa-book"></i><span>História das bebidas</span></a>
-            <a href="audit.php"><i class="fas fa-book"></i><span>Teste Audit</span></a> 
-        </div>
-    </li>
-    <li= class="item" id="settings">
-        <a href="#settings" class="menu-btn">
-            <i class="fas fa-cog"></i><span>Configurações<i class="fas fa-chevron-down drop-down"></i></span>
-        </a>
-        <div class="sub-menu">
-            <a href="#"><i class="fas fa-lock"></i><span>Senha</span></a>
-            
-        </div>
-    </li>
-    <li class="item">
-        <a href="#" class="menu-btn">
-            <i class="fas fa-info-circle"></i><span>Sobre</span>
-        </a>
-    </li>
-</div>
-</div>
-</div>
-<!--sidebar end-->
 
-<!--wrapper end-->
-<!-- fazer menu se mexer>>
-    
-    <script type="text/javascript">
-$(document).ready(function(){
-    $(".sidebar-btn").click(function(){
-        $(".wrapper").toggleClass("collapse");
-    
-    });
+        <h1>Olá! Seja bem vindo(a)!</h1>
+        <div class="container">
+            <div class="row">
+         <!--CARD 1 - HISTORIA DAS BEBIDAS - YOUTUBE-->       
+            <div class="col-xs-6 col-sm-4">
+            <div class="card">
+            <a class="img-card" href="https://www.ted.com/talks/rod_phillips_a_brief_history_of_alcohol/transcript?language=pt-br#t-8168"><img src="./imagens/historia-das-bebidas-.png" /></a>
+            <br/>
+            <div class="card-content">
+            <h4 class="card-title">
+            <a href="https://www.ted.com/talks/rod_phillips_a_brief_history_of_alcohol/transcript?language=pt-br#t-8168"> HISTÓRIA DAS BEBIDAS </a>
+            </h4>
+            <div class="">Você sabe como as bebidas alcoólicas surgiram? Aqui nesse vídeo, Rod Phillips explica essa história por meio de uma animacão. Venha conferir!</div>
+            </div>
+            <div class="card-read-more">
+            <a class="btn btn-link btn-block" href="https://www.ted.com/talks/rod_phillips_a_brief_history_of_alcohol/transcript?language=pt-br#t-8168">Clique aqui para saber mais</a>
+            </div>
+            </div>
+            </div>
 
-});
-</script>-->
+            <!--CARD 2 - QUIZ - LINK DO QUIZ -->   
+            <div class="col-xs-6 col-sm-4">
+                <div class="card">
+                <a class="img-card" href="./Quiz/quiz.html"><img src="./imagens/imagem-quiz.png" /></a>
+                <br/>
+                <div class="card-content">
+                <h4 class="card-title">
+                <a href="./Quiz/quiz.html"> QUIZ </a>
+                </h4>
+                <div class="">E ai, já tomou um gole de consciência no nosso site? Entao venha responder nosso Quiz, marcar muitos pontos e escalar nosso ranking!</div>
+                </div>
+                <div class="card-read-more">
+                <a class="btn btn-link btn-block" href="./Quiz/quiz.html">Clique aqui para saber mais</a>
+                </div>
+                </div>
+                </div>
+
+        
+              <!--CARD 3 - TESTE AUDIT - LINK DO AUDIT-->   
+            <div class="col-xs-6 col-sm-4">
+             <div class="card">
+            <a class="img-card" href="./audit.php"><img src="./imagens/imagem-teste-audit.png" /></a>
+            <br/>
+            <div class="card-content">
+            <h4 class="card-title">
+            <a href="./audit.php"> TESTE AUDIT </a>
+            </h4>
+            <div class="">O AUDIT - Alcool Use Disorders Identification Test é um instrumento de avaliação desenvolvido pela Organização Mundial de Saúde (OMS). </div>
+            </div>
+            <div class="card-read-more">
+            <a class="btn btn-link btn-block" href="./audit.php">Clique aqui para saber mais</a>
+            </div>
+            </div>
+            </div>
 
 
+              <!--CARD 4 - EFEITO DA BEBIDA NO CORPO HUMANO-->   
+            <div class="col-xs-6 col-sm-4">
+                <div class="card">
+                <a class="img-card" href="https://super.abril.com.br/mundo-estranho/como-o-organismo-reage-ao-alcool/ "><img src="./imagens/imagem-efeitos-da-bebida.jpeg" /></a>
+                <br/>
+                <div class="card-content">
+                <h4 class="card-title">
+                <a href="https://super.abril.com.br/mundo-estranho/como-o-organismo-reage-ao-alcool/ "> COMO O ORGANISMO REAGE AO ÁLCOOL? </a>
+                </h4>
+                <div class="">Você já se perguntou como o corpo reage ao álcool ingerido? A revista Mundo Estranho conta o que acontece em vários órgãos do nosso organismo.</div>
+                </div>
+                <div class="card-read-more">
+                <a class="btn btn-link btn-block" href="https://super.abril.com.br/mundo-estranho/como-o-organismo-reage-ao-alcool/ ">Clique aqui para saber mais</a>
+                </div>
+                </div>
+                </div>
 
-<!-- teste de cards-->
-<section class="service py-3">
-        <div class="col mx-auto align-items-center my-5 ">
-    <div class="heading text-center pt-5">
-      <h1 class="font-weight-bold pb-5 text-warning center">teste</h2>
-    </div>
+                  <!--CARD 5 - INFLUENCIA DAS MIDIAS -->   
+                <div class="col-xs-6 col-sm-4">
+                    <div class="card">
+                    <a class="img-card" href="https://cisa.org.br/index.php/sua-saude/informativos/artigo/item/248-influencias-das-midias-sociais-no-consumo-de-alcool-entre-adolescentes-e-jovens"><img src="./imagens/imagem-influencia-das-bebidas.png" /></a>
+                    <br/>
+                    <div class="card-content">
+                    <h4 class="card-title">
+                    <a href="https://cisa.org.br/index.php/sua-saude/informativos/artigo/item/248-influencias-das-midias-sociais-no-consumo-de-alcool-entre-adolescentes-e-jovens"> MÍDIAS SOCIAIS E SUA INFLUÊNCIA </a>
+                    </h4>
+                    <div class="">Você acredita no poder da influência das mídias? Aqui você pode ver como as mídias sociais podem influenciar o comportamento dos jovens em relação às bebidas alcoólicas.</div>
+                    </div>
+                    <div class="card-read-more">
+                    <a class="btn btn-link btn-block" href="https://cisa.org.br/index.php/sua-saude/informativos/artigo/item/248-influencias-das-midias-sociais-no-consumo-de-alcool-entre-adolescentes-e-jovens">Clique aqui para saber mais</a>
+                    </div>
+                    </div>
+                    </div>
 
-    <div class="row mx-auto justify-content-center align-items-center text-center container">
-      <div class="one col-lg-3 col-md-3 col-12 w-25 m-2">
-        <img class="img-fluid w-90" src="./imagens/card1 quiz.svg">
-        <h5 class="font-weight-bold pt-4">TEMOS QUIZ</h5>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-    </div>
-
-    <div class="one col-lg-3 col-md-3 col-12 w-25 m-2">
-      <img class="img-fluid w-90" src="./imagens/ranking.svg">
-      <h5 class="font-weight-bold pt-4">RANKINGS</h5>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-  </div>
-
-  <div class="one col-lg-3 col-md-3 col-12 w-25 m-2">
-    <img class="img-fluid w-90 " src="./imagens/card2 audit.svg">
-    <h5 class="font-weight-bold pt-4">TESTE AUDIT</h5>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-</div>
-    </div>
-  </div>
-  </section>
-
- <!--Links essenciais do CDN bootstrap, jquery e javascript que faz a magia funcionar -->    
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
- 
- 
- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>    
-
-
-</body>
+                      <!--CARD 6 - METABOLISMO DO ALCOOL-->   
+                    <div class="col-xs-6 col-sm-4">
+                        <div class="card">
+                        <a class="img-card" href="https://cisa.org.br/index.php/sua-saude/informativos/artigo/item/47-metabolismo-do-alcool"><img src="./imagens/imagem-metabolismo-do-alcool.png" /></a>
+                        <br/>
+                        <div class="card-content">
+                        <h4 class="card-title">
+                        <a href="https://cisa.org.br/index.php/sua-saude/informativos/artigo/item/47-metabolismo-do-alcool"> METABOLISMO DO ÁLCOOL </a>
+                        </h4>
+                        <div class=""> Você sabia que da ingestão até ser eliminado, o álcool passa por diversas fases de metabolização dentro do nosso organismo?</div>
+                        </div>
+                        <div class="card-read-more">
+                        <a class="btn btn-link btn-block" href="https://cisa.org.br/index.php/sua-saude/informativos/artigo/item/47-metabolismo-do-alcool">Clique aqui para saber mais</a>
+                        </div>
+                        </div>
+                        </div>
+                </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+        <script src="main-moodle.js"></script>
+    </body>
 </html>
