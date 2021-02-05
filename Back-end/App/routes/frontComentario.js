8ad2f39623728b138796ea9a5fe88d1be1740b89
Sabrina-AP <72463853+Sabrina-AP@users.noreply.github.com>
2021-02-05 19:04:14 -0300

    .

--- Back-end/App/routes/frontComentario.js
+++ Back-end/App/routes/frontComentario.js
@@ -1,4 +1,22 @@
 module.exports = function (app){
+    // register_coments.php
+    
+    app.post('/comentarios/envia', function(req,res){ 
+        var comentario = req.body;
+        var connection = app.app.config.database;
+        var comentarioModel = app.app.models.comentarioModel;
+        comentarioModel.setComentario(comentario,  connection, 
+            function(error,results){
+            console.log(error, results)
+        });
+        comentarioModel.getComentario(connection, 
+            function(error, results, fields){
+                console.log(error,results)
+                res.json(results);                    
+        });   
+    });
+    
+    
     app.get('/comentarios/retorna', function(req,res){
         var connection = app.app.config.database;
         var query = app.app.models.comentarioModel;
