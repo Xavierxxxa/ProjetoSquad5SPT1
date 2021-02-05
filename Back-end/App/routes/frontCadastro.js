26ad329dd150555364e49489434eb07c98866611
Sabrina-AP <72463853+Sabrina-AP@users.noreply.github.com>
2021-02-05 19:02:35 -0300

    .

--- Back-end/App/routes/frontCadastro.js
+++ Back-end/App/routes/frontCadastro.js
@@ -1,4 +1,22 @@
 module.exports = function (app){
+    
+    // SQLInsert.php ----------------------------------- // excluirConta.php
+
+    app.post('/cadastrado/envia', function(req,res){
+        var cadastrado = req.body;
+        var connection = app.app.config.database;
+        var cadastroModel = app.app.models.cadastroModel;
+        cadastroModel.setCadastro(cadastrado, connection, function(error,results){
+            console.log(error, results)
+        });
+        cadastroModel.getCadastro(connection, 
+            function(error, results, fields){
+                console.log(error,results)
+                res.json(results);   
+            }
+        );
+    });
+    
     app.get('/cadastrado/retorna', function(req,res){
         var connection = app.app.config.database;
         var query = app.app.models.cadastroModel;
